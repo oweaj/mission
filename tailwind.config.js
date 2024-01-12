2185900a89d8877fced05105d2d16d1973b8d4bc
oweaj <qwe1520156@naver.com>
2024-01-12 23:15:04 +0900

    [SETTING] package 구성

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'home-color': '#B5B5B5',
+        'order-button': '#C1C1C1',
+        'order-color': '#F75A2F',
+      },
+    },
+  },
+  plugins: [],
+};
