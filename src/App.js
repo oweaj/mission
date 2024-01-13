473d92f48d342a84d23bba369a345edb9037dc85
oweaj <qwe1520156@naver.com>
2024-01-13 23:49:42 +0900

    [STYLE] position fixed로 수정

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="w-[350px] h-screen absolute left-1/2 -translate-x-1/2 text-lg bg-white">
+    <div className="w-[350px] h-screen fixed left-1/2 -translate-x-1/2 text-lg bg-white">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/order" element={<Order />} />
