b9fb9eb0531c96bb8de7fb14477bd0b445896ddf
oweaj <qwe1520156@naver.com>
2024-01-15 18:17:16 +0900

    [FIX] 상품 주문 post요청 여부에 따른 페이지 이동

--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -1,22 +1,30 @@
 import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { totalCountSelector, totalPriceSelector } from '../../atom/selectItemAtom';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
+import { selectItemAtom, totalCountSelector, totalPriceSelector } from '../../atom/selectItemAtom';
+
 import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Total = () => {
   const [active, setActive] = useState(false);
   const totalCount = useRecoilValue(totalCountSelector);
   const totalPrice = useRecoilValue(totalPriceSelector);
+  const orderItem = useRecoilValue(selectItemAtom);
+  const selectItemReset = useResetRecoilState(selectItemAtom);
   const navigate = useNavigate();
 
-  const handleOrderButton = async () => {
+  // 선택한 상품 주문하기
+  const handleOrder = async () => {
     setActive(true);
-    if (totalCount) {
+    try {
+      const url = 'http://localhost:3001/orders';
+      await axios.post(url, orderItem);
       navigate('/complete');
-    } else {
+    } catch (error) {
       navigate('/error');
     }
+    selectItemReset();
   };
 
   return (
@@ -25,7 +33,7 @@ const Total = () => {
         <p>총 수량 : {totalCount}개</p>
         <p>총 가격 : {totalPrice.toLocaleString()}원</p>
       </div>
-      <Button count={totalCount} active={active} onClick={handleOrderButton} />
+      <Button count={totalCount} active={active} onClick={handleOrder} />
     </div>
   );
 };
